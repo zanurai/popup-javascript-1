7ced0a94821a7514960a24822e517a20a494ec5d
Zanu-bit <113513397+Zanu-bit@users.noreply.github.com>
2022-10-11 13:20:34 +0545

    initilize commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+
+
+function openPopup() {
+    popup.classList.add("openpop");
+}
+
+function closePopup() {
+    popup.classList.remove("openpop");
+}
